8cdade5cc768049999ad23f1de9880ff373b6850
Igor <igor.pogosian@small-team.com>
2016-09-21 08:34:53 +0300

    added get country name helpers

--- app/helpers/full_country_select/api_helper.rb
+++ app/helpers/full_country_select/api_helper.rb
@@ -13,5 +13,17 @@ module FullCountrySelect
       return {} unless obj.country
       CS.get(obj.country, obj.state).map.with_index{|v, k| [v, k]}
     end
+
+    def country_name(obj)
+      CS.get[obj.country.to_sym]
+    end
+
+    def state_name(obj)
+      CS.get(obj.country)[obj.state.to_sym] if obj.country
+    end
+
+    def city_name(obj)
+      CS.get(obj.country, obj.state)[obj.city] if obj.country
+    end
   end
 end
